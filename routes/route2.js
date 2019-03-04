f535ca80ab74bf84f971fe9266d08d4a03fc2a1b
siergieyk <siergieyk@gmail.com>
2019-03-03 20:00:31 -0800

    IA7

--- routes/route2.js
+++ routes/route2.js
@@ -1,9 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var colors = require("../colors.json");
 
-const routes = require('express').Router();
+router.get('/all', function(req, res, next){
+    res.send(colors);
+})
 
-
-routes.get('/', (req, res) => {
-  res.status(200).json({ message: 'Connected!' });
+router.get('/:color', function(req, res, next){
+    var myColor = getColorByName(req.params.color.toLowerCase().toString());
+    res.send(myColor);
 });
 
-module.exports = routes;
+function getColorByName(key){
+    for(var i = 0; i < colors.colors.length; i++){
+        if(colors.colors[i].color === key){
+        return colors.colors[i];
+        
+    }else{
+        res.status(404).send('Error 404 Not found');
+    }
+    }
+}
+module.exports = router;
